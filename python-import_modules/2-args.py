4664fbaef4b031718139403d6a610aa4341f4fff
LianisDevs <lianisdevs@gmail.com>
2026-01-24 20:17:47 +1100

    fix: runs only as script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-length = len(sys.argv)
-arguments = length - 1
+if __name__ == "__main__":
+    length = len(sys.argv)
+    arguments = length - 1
 
-if arguments == 0:
-    print("0 arguments.")
-elif arguments == 1:
-    print(arguments, "argument:")
-    print("1:", sys.argv[1])
-else:
-    i = 1
-    print(arguments, "arguments:")
-    while (i < length):
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print(arguments, "argument:")
+        print("1:", sys.argv[1])
+    else:
+        i = 1
+        print(arguments, "arguments:")
+        while (i < length):
+            print("{}: {}".format(i, sys.argv[i]))
+            i += 1
