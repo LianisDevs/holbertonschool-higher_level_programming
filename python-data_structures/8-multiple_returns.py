e82e53de3576a3c002beacebe3e5c364c8a5dffb
LianisDevs <lianisdevs@gmail.com>
2026-01-31 09:01:28 +1100

    fix: handles empty string

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,8 +4,9 @@ def multiple_returns(sentence):
     if sentence == None:
         tuple = (0, None)
 
-    length = len(sentence)
-    char = sentence[0]
-    tuple = (length, char)
+    else:
+        length = len(sentence)
+        char = sentence[0]
+        tuple = (length, char)
 
     return tuple
