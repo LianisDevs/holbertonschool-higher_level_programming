13b4c9bdbc7144d3c8589e7ebbf97e4e8786b37c
LianisDevs <lianisdevs@gmail.com>
2026-01-31 08:57:42 +1100

    add: task 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence == None:
+        tuple = (0, None)
+
+    length = len(sentence)
+    char = sentence[0]
+    tuple = (length, char)
+
+    return tuple
