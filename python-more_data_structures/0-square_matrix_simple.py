17f09d9aa0ccc9424fdc9cbbb121e01e3c303af4
LianisDevs <lianisdevs@gmail.com>
2026-01-31 09:36:28 +1100

    fix: pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_it = lambda num: num * num
     squared_matrix = [list(map(square_it, row)) for row in matrix]
-
     return squared_matrix
+
+
+def square_it(num):
+    return num * num
