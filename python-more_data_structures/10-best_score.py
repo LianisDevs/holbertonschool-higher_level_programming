5883220aa9acb20347ff0963f47e988602eec30b
LianisDevs <lianisdevs@gmail.com>
2026-01-31 18:43:35 +1100

    fix: handles empty dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if not a_dictionary:
+        return None
+    else:
         max_value_key = max(a_dictionary, key=a_dictionary.get)
         return max_value_key
-    else:
-        return None
