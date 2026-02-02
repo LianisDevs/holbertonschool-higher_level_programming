f9b861a1c2061b7a4416cb73ee0f3193c30a4ff3
LianisDevs <lianisdevs@gmail.com>
2026-02-02 12:06:33 +1100

    add: task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is the module containing the square class"""
+
+
+class Square:
+    """
+    This is the Square class
+
+    Parameters:
+    size: this is the size of the square
+    """
+
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
