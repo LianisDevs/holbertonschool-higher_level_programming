586b5cb5c1956c7beec922d822efca68057cf352
LianisDevs <lianisdevs@gmail.com>
2026-02-02 16:20:56 +1100

    add: task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This is the module containing the square class"""
+
+
+class Square:
+    """
+    This is the Square class
+
+    Parameters:
+    size: this is the size of the square
+    """
+
+    __size = 0
+
+    # called when a new square is made
+    def __init__(self, size=0):
+        self.size = size
+
+    # getter
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
