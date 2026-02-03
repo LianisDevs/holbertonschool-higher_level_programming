754a6f3881316f893a57268b31bf617511816765
LianisDevs <lianisdevs@gmail.com>
2026-02-03 19:56:57 +1100

    add: task 3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This is the module containing the rectangle class"""
+
+
+class Rectangle:
+    """
+    This is the Rectangle class
+
+    Parameters:
+    width: this is the width of the rectangle
+    height: this is the height of the rectangle
+    area: this is the area of the rectangle (width * height)
+    perimeter: this is the perimeter of the rectangle (width + height)
+    """
+
+    # called when a new rectangle is made
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    # getter
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif not value >= 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    # getter
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif not value >= 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    # for print()
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join("#" * self.__width for x in range(self.__height))
