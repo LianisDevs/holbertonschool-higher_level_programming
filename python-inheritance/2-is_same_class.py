7a7a96502aee24e8bd0bfb80294477b7643a8a68
LianisDevs <lianisdevs@gmail.com>
2026-02-09 16:28:12 +1100

    add: task 2:

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This is the 2-is_same_class module."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if obj is exactly an instance of a_class
+
+    Parameters:
+    obj : object to check
+    a_class: class to check if obj is an instance of
+
+    Returns:
+    True or False
+
+    """
+    if not type(obj) == a_class:
+        return False
+    return True
