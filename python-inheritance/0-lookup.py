f6c0c9126f0e789938cb5af908d752d962feca9c
LianisDevs <lianisdevs@gmail.com>
2026-02-09 11:38:54 +1100

    fix: documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+This is the 0-lookup module.
+"""
+
+
 def lookup(obj):
-    return dir(obj)
+    """
+    Accesses available attributes and methods of an object
+
+    Parameters:
+    obj : object to access attributes and methods of
+
+    Returns:
+    list: available attributes and methods of object
+
+    """
+    dir_list = [dir(obj)]
+    return dir_list
