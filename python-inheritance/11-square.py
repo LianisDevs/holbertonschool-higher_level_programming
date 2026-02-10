3884a99347d7dc983d04f1965e791989fac99d3e
LianisDevs <lianisdevs@gmail.com>
2026-02-10 16:47:49 +1100

    add: task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is the module containing Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is the Square class
+
+    Parameters:
+    Rectangle: inherits Rectangle class
+
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[{__class__.__name__}] {self.__size}/{self.__size}"
+
+    def area(self):
+        area = self.__size ** 2
+        return area
