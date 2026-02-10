e43dcff6b937e308ccff00ddd3760b4f21818647
LianisDevs <lianisdevs@gmail.com>
2026-02-10 15:21:53 +1100

    fix: error bool value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if isinstance(value, int) and not isinstance(value, bool):
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+        else:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
