d56a78cba20483e58a4a6644e88742afef4af248
LianisDevs <lianisdevs@gmail.com>
2026-02-12 14:54:33 +1100

    add: task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """
+    This is the Shape class
+
+    Parameters: inherits ABC
+    """
+
+    @abstractmethod
+    def area(self):
+        # each child class must implement this abstractmethod
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        # each child class must implement this abstractmethod
+        pass
+
+
+class Circle(Shape):
+    """
+    This is the Circle class
+
+    Parameters: inherits Shape
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    This is the Rectangle class
+
+    Parameters: inherits Shape
+    """
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(obj):
+    print("Area: {}".format(obj.area()))
+    print("Perimeter: {}".format(obj.perimeter()))
