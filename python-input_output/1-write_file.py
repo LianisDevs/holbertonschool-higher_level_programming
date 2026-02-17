9c36984c62ec8bdb9ba63d1198582db4a386b5c5
LianisDevs <lianisdevs@gmail.com>
2026-02-17 16:10:48 +1100

    fix: function documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,10 @@ This is the module containing the write_file function
 
 
 def write_file(file_name="", text=""):
+    """
+    This is the write_file function
+
+    Parameters: file_name and text
+    """
     with open(file_name, "w") as file:
         file.write(text)
