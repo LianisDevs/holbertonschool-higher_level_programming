4ff0e1befdb9b8c07243b4f99f4ff1fccaac2d45
LianisDevs <lianisdevs@gmail.com>
2026-02-21 18:42:29 +1100

    add: task and test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""This is the Student module"""
+
+
+class Student():
+    """
+    Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            if isinstance(attrs, list):
+                if all(isinstance(item, str) for item in attrs):
+                    result = self.__dict__
+                    return {key: value for key, value
+                            in result.items() if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json: dict):
+        vars(self).update(json)
---
+++ python-input_output/11-test.py
@@ -0,0 +1,46 @@
+import pytest
+Student = __import__('11-student').Student
+
+class TestStudentClass():
+    def test_student_class_exists(self):
+        student = Student("John", "Doe", 23)
+        assert isinstance(student, Student)
+
+    def test_student_has_to_json(self):
+        student = Student("John", "Doe", 23)
+        result = student.to_json()
+        assert isinstance(result, dict)
+        assert "first_name" in result
+
+    def test_student_to_json_attr_check(self):
+        student = Student("John", "Doe", 23)
+        result = student.to_json("age")
+        assert "age" in result
+
+    def test_student_to_json_attr_check_multi_attrs(self):
+        student = Student("John", "Doe", 23)
+        result = student.to_json(["age", "first_name"])
+        assert "age" in result
+        assert "first_name" in result
+
+    def test_student_to_json_handles_invalid_input(self):
+        student = Student("John", "Doe", 23)
+        result = student.to_json([1, 2, 3])
+        assert "age" in result
+        assert "first_name" in result
+        assert "last_name" in result
+
+    def test_student_to_json_handles_invalid_input_with_valid_input(self):
+        student = Student("John", "Doe", 23)
+        result = student.to_json([1, "age", "first_name"])
+        assert "age" in result
+        assert "first_name" in result
+        assert "last_name" in result
+
+    def test_student_has_reload_from_json(self):
+        student = Student("John", "Doe", 23)
+        student.reload_from_json({"first_name": "Jane", "last_name": "Smith", "age": 44})
+        assert student.first_name == "Jane"
+        assert student.last_name == "Smith"
+        assert student.age == 44
+
