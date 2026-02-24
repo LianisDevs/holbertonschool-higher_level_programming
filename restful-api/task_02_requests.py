a48b049868197bbfdc0bb2eedfd11fef20a3077e
LianisDevs <lianisdevs@gmail.com>
2026-02-24 22:01:34 +1100

    fix: csv conversion

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,12 +6,13 @@ Contains fetch_and_print_posts() and fetch_and_save_posts()
 """
 
 import requests
-import json
+import csv
 
 
 def fetch_and_print_posts():
     posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     print("Status Code: {}".format(posts.status_code))
+    print(posts.headers)
 
     if posts.status_code == 200:
         posts_obj = posts.json()
@@ -23,8 +24,15 @@ def fetch_and_save_posts():
     posts = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     if posts.status_code == 200:
+        posts_obj = posts.json()
+        if posts_obj:
+            fieldnames = posts_obj[0].keys()
+        else:
+            fieldnames = []
         try:
-            with open("posts.csv", "w", encoding="utf-8") as file:
-                file.write(json.dumps(posts))
+            with open("posts.csv", "w", newline='', encoding='utf-8') as file:
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(posts_obj)
         except Exception:
             pass
