7d3c38085dce081bc3d380a3fdf4c91dd6dab8b0
LianisDevs <lianisdevs@gmail.com>
2026-03-01 16:10:58 +1100

    fix: set database

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,11 @@
 --  lists all the cities of California that can be found in the database
+SET @db = SELECT DATABASE();
 
 SELECT c.id, c.name
-FROM hbtn_0d_usa.cities AS c
+FROM @db.cities AS c
 WHERE c.state_id = (
 	SELECT s.id
-	FROM hbtn_0d_usa.states as s
+	FROM @db.states as s
 	WHERE s.name = "California"
 )
-
 ORDER BY c.id;
