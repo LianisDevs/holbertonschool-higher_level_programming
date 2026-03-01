6b5d046b32287dc8847cfce45cde2c94664c710f
LianisDevs <lianisdevs@gmail.com>
2026-03-01 12:13:03 +1100

    fix: added IGNORE to handle duplication

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 
 -- inserts multiple records into table
-INSERT INTO second_table(id, name, score) VALUES
+INSERT IGNORE INTO second_table(id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
