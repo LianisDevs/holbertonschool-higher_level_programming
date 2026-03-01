4ad5619c2690bf9f94a41d1085e22d93bdb701ee
LianisDevs <lianisdevs@gmail.com>
2026-03-01 15:09:56 +1100

    add: task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- creates the table states in the database hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTOINCREMENT PRIMARY KEY,
+	name VARCHAR(256) IS NOT NULL,
+);
