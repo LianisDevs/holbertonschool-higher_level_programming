ffc91309438ae42d2b56afd206d3c353ab26bd3b
LianisDevs <lianisdevs@gmail.com>
2026-03-01 13:01:53 +1100

    fix: flush to ensure changes take effect

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,12 @@
 
 -- create db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
 -- grant privileges to user
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- reload privileges to ensure changes take effect
+FLUSH PRIVILEGES;
