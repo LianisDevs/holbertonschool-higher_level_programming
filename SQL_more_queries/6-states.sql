278e9992f9648dc80a5aa33234959a296cf80475
LianisDevs <lianisdevs@gmail.com>
2026-03-01 15:18:28 +1100

    fix: typo in auto_increment

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- creates the table states in the database hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS states (
-	id INT AUTOINCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
