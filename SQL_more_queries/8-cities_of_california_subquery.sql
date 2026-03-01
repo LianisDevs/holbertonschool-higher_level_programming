82c13aaafe55ff692f18971e1721fb92fbbcc79c
LianisDevs <lianisdevs@gmail.com>
2026-03-01 16:15:11 +1100

    fix: removed databse specify

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,9 @@
 --  lists all the cities of California that can be found in the database
-SET @db = SELECT DATABASE();
 
 SELECT c.id, c.name
-FROM @db.cities AS c
+FROM cities AS c
 WHERE c.state_id = (
 	SELECT s.id
-	FROM @db.states as s
-	WHERE s.name = "California"
-)
+	FROM states as s
+	WHERE s.name = "California")
 ORDER BY c.id;
