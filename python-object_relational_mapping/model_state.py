52e6ab1aca4ceee6431421def3e0bc7a61d6e326
LianisDevs <lianisdevs@gmail.com>
2026-03-05 21:52:11 +1100

    add: task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the State class
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State Class inherits Base class """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
