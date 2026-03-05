d10c60b08cd4e0e632e687e2772d610ba95cf710
LianisDevs <lianisdevs@gmail.com>
2026-03-05 17:09:52 +1100

    add: task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module contains the get_user_input_from_database function
+"""
+
+import sys
+import MySQLdb
+
+
+def get_user_input_from_database():
+    """
+    This function connects to a MySQL db
+    Performs SQL query to select states starting with N from the table
+    Prints each row from selection
+    """
+    my_db = MySQLdb.connect(
+        host='127.0.0.1',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+
+    cur = my_db.cursor()
+
+    sql_query = (
+        "SELECT * FROM states \
+        WHERE name = %s \
+        ORDER BY id ASC")
+
+    cur.execute(sql_query, (sys.argv[4],))
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    my_db.close()
+
+
+if __name__ == "__main__":
+    get_user_input_from_database()
