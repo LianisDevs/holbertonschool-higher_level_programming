e59139e5624ea31aa8dc106b196b8f75d7669d0c
LianisDevs <lianisdevs@gmail.com>
2026-03-06 22:05:59 +1100

    fix: localhost port

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -6,7 +6,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine, insert
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@172.17.0.4/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
 
