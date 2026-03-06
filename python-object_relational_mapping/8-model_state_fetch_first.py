de1ee13b5275300e3351d206ca0f9bc2fe2cc843
LianisDevs <lianisdevs@gmail.com>
2026-03-06 21:34:45 +1100

    add: task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Select first row from states from database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    with engine.connect() as conn:
+        query = select(State)
+
+        i = conn.execute(query).first()
+        print("{}: {}".format(i[0], i[1]))
