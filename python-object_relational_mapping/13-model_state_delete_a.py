d2293ef94431e1f95a239270307adccc53f49403
LianisDevs <lianisdevs@gmail.com>
2026-03-06 22:26:14 +1100

    add: task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" changes the name of a State object from the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, delete
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    with engine.connect() as conn:
+        query = delete(State).filter(State.name.contains("a"))
+
+        query.compile()
+        i = conn.execute(query)
