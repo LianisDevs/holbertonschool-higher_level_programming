8347b78f257b2b82acfbbef117868dff11c860cd
LianisDevs <lianisdevs@gmail.com>
2026-03-06 22:19:45 +1100

    add: task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" changes the name of a State object from the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, update
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@172.17.0.4/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    with engine.connect() as conn:
+        query = update(State).where(State.id == 2).values(name="New Mexico")
+
+        query.compile()
+        i = conn.execute(query)
+        conn.commit()
