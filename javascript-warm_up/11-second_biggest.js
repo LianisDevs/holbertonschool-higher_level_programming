a6ab122c1890ddb6abddb65f1c43e88a19e30ac3
LianisDevs <lianisdevs@gmail.com>
2026-03-18 17:17:40 +1100

    fix: removed debugging print statements

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,18 +6,15 @@ let max = -Infinity;
 let secondMax = -Infinity;
 
 if (args.length < 2) {
-  console.log(0);
+	console.log(0);
 } else {
-  for (const num of args) {
-    if (num > max) {
-      console.log('num is', num);
-      secondMax = max;
-      console.log('secondMax is now ', secondMax);
-      max = num;
-      console.log('max is now', max);
-    } else if (num > secondMax && num < max) {
-      secondMax = num;
-    }
-  }
-  console.log(secondMax);
+	for (const num of args) {
+		if (num > max) {
+			secondMax = max;
+			max = num;
+		} else if (num > secondMax && num < max) {
+			secondMax = num;
+		}
+	}
+	console.log(secondMax);
 }
