d5330fafabf060c6a8799d2611dac71ed8dbdeb0
LianisDevs <lianisdevs@gmail.com>
2026-03-18 17:18:24 +1100

    fix: semistandard format

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,15 +6,15 @@ let max = -Infinity;
 let secondMax = -Infinity;
 
 if (args.length < 2) {
-	console.log(0);
+  console.log(0);
 } else {
-	for (const num of args) {
-		if (num > max) {
-			secondMax = max;
-			max = num;
-		} else if (num > secondMax && num < max) {
-			secondMax = num;
-		}
-	}
-	console.log(secondMax);
+  for (const num of args) {
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax && num < max) {
+      secondMax = num;
+    }
+  }
+  console.log(secondMax);
 }
