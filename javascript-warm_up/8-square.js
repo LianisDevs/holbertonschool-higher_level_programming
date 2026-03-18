680cfdbf6e183bba56b7ff34697933095f09e03e
LianisDevs <lianisdevs@gmail.com>
2026-03-18 16:09:42 +1100

    add: task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv;
+
+const num = Number(args[2]);
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let a = 0; a < num; a++) {
+    let row = '';
+
+    for (let b = 0; b < num; b++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+}
