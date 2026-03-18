84405a4c5019c0ef56aa9b146cc41cfae7ddb479
LianisDevs <lianisdevs@gmail.com>
2026-03-18 16:12:54 +1100

    fix: capital X to print

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,14 +4,14 @@ const args = process.argv;
 const num = Number(args[2]);
 
 if (isNaN(num)) {
-  console.log('Missing size');
+	console.log('Missing size');
 } else {
-  for (let a = 0; a < num; a++) {
-    let row = '';
+	for (let a = 0; a < num; a++) {
+		let row = '';
 
-    for (let b = 0; b < num; b++) {
-      row += 'x';
-    }
-    console.log(row);
-  }
+		for (let b = 0; b < num; b++) {
+			row += 'X';
+		}
+		console.log(row);
+	}
 }
