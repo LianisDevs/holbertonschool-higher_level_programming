7c0a44efab0a2d7f4107508d1066e34a32303bde
LianisDevs <lianisdevs@gmail.com>
2026-03-18 17:16:47 +1100

    fix: correct output

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 
-const args = process.argv;
+const args = process.argv.slice(2).map(Number);
 
-let max = 0;
-let secondMax = 0;
+let max = -Infinity;
+let secondMax = -Infinity;
 
-if (args[2] === undefined || args[3] === undefined) {
+if (args.length < 2) {
   console.log(0);
 } else {
   for (const num of args) {
     if (num > max) {
+      console.log('num is', num);
       secondMax = max;
+      console.log('secondMax is now ', secondMax);
       max = num;
-    } else if (num > secondMax) {
+      console.log('max is now', max);
+    } else if (num > secondMax && num < max) {
       secondMax = num;
     }
   }
