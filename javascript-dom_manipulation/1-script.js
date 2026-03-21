7d3311684f5848eec3e9dd5069dea5ada5cd6993
LianisDevs <lianisdevs@gmail.com>
2026-03-21 19:15:10 +1100

    add: task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const headerElement = document.getElementById('red_header');
+
+const redHeaderElement = document.querySelector('header');
+
+redHeaderElement.addEventListener('click', function(event) {
+	headerElement.style.color = '#FF0000';
+});
